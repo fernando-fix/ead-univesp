67cf769c992cd9f421f37f52b2e140fb71438d9f
Fernando Rodrigues <ferando_tecti@hotmail.com>
2025-08-24 09:21:39 -0300

    Adicionado code splitting

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,24 +2,34 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { lazy } from 'react';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => {
-        const modules = {
-            ...import.meta.glob('./Admin/Pages/**/*.tsx'),
-            ...import.meta.glob('./Teachers/Pages/**/*.tsx'),
-            ...import.meta.glob('./Students/Pages/**/*.tsx'),
-            ...import.meta.glob('./Web/Pages/**/*.tsx'),
+        // Usando importações dinâmicas para carregamento preguiçoso
+        const page = () => {
+            const modules = {
+                ...import.meta.glob('./Admin/Pages/**/*.tsx'),
+                ...import.meta.glob('./Teachers/Pages/**/*.tsx'),
+                ...import.meta.glob('./Students/Pages/**/*.tsx'),
+                ...import.meta.glob('./Web/Pages/**/*.tsx'),
+            };
+
+            const module = modules[`./${name}.tsx`];
+            if (!module) throw new Error(`Página não encontrada: ${name}`);
+
+            const Component = lazy(() => module().then(component =>
+                ({ default: component.default })
+            ));
+
+            return Component;
         };
-        return resolvePageComponent(
-            `./${name}.tsx`,
-            modules
-        );
+
+        return page();
     },
     setup({ el, App, props }) {
         const root = createRoot(el!);
